c58431980e56198b5f895f16afd30976f654ebc7
lcprojects <43647132+lcprojects@users.noreply.github.com>
2020-05-01 18:35:37 +0100

    Updates

---
+++ precache-manifest.ba2e6fc2cff61897b771fd8416be235f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a421baa378969b429480cab3a8e75bc8",
+    "url": "/pickles/index.html"
+  },
+  {
+    "revision": "525bfa1b4fdb576c2ee9",
+    "url": "/pickles/static/css/main.bbb642f9.chunk.css"
+  },
+  {
+    "revision": "02930cc4cba9713f53bf",
+    "url": "/pickles/static/js/2.7a58ae66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pickles/static/js/2.7a58ae66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525bfa1b4fdb576c2ee9",
+    "url": "/pickles/static/js/main.1cf12eb7.chunk.js"
+  },
+  {
+    "revision": "2f186570c581c706452d",
+    "url": "/pickles/static/js/runtime-main.2ea872b1.js"
+  }
+]);
